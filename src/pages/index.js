1fca196dc8f5f2878e324f0cd4b0f6345bd587d8
shawnyama <shawn.yama@ontariotechu.net>
2022-02-19 23:54:31 -0500

    add title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Nav from '../components/Nav.js'
 import About from '../components/About.js'
 import Projects from '../components/Projects.js'
 import Footer from '../components/Footer.js'
+import favicon from '../images/favicon.ico'
 import '../styles/index.css'
 
 const IndexPage = () => {
@@ -11,7 +12,9 @@ const IndexPage = () => {
 		<main>
 			<Helmet>
 				<meta name="description" content="Shawn Yama's portfolio" />
+				<link rel="icon" href={favicon} />
 				<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"></link>
+				<title>Shawn Yama</title>
 			</Helmet>
 			<Nav></Nav>
 			<About></About>
