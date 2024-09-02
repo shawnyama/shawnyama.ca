daf9bdf9db75999b64452324a223f9532eca53d2
Shawn Yama <syama@uncharted.software>
2024-09-02 14:58:47 -0400

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://shawnyama.ca'
+  site: 'https://shawnyama.ca',
+  integrations: [partytown()]
 });
